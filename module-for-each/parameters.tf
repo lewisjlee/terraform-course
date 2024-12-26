478c4c24d5926575312638c8473e3a6c17b16e98
LewisJLee <earthlive1102@gmail.com>
2024-12-26 13:42:27 +0900

    module 실습

--- module-for-each/parameters.tf
+++ module-for-each/parameters.tf
@@ -1,3 +1,5 @@
+# 정적 변수나 map, list를 선언할 때 local 구문을 사용
+# 여기는 my_parameters라는 map 정의
 locals {
   my_parameters = {
     environment = "development"
@@ -13,6 +15,7 @@ module "parameters" {
   value    = each.value
 }
 
+# 하위 ssm-parameter 모듈의 출력값은 arn이다.
 output "all-my-parameters" {
   value = { for k, parameter in module.parameters: k => parameter.arn }
 }
